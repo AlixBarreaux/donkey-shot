3613aebff27e463efa1cd897421946eac7e87461
BarreauxAlix <barreauxalix@tutanota.com>
2021-11-06 18:40:34 +0100

    Make the in game music stop playing when the game is over and play again from the start when the game restarted

--- Scripts/in_game_music_player.gd
+++ Scripts/in_game_music_player.gd
@@ -7,7 +7,9 @@ extends AudioStreamPlayer
 
 # Signals to connect to
 onready var signals_connections_list: PoolIntArray = [
-	Events.connect("game_started", self, "play", [0.0])
+	Events.connect("game_started", self, "play", [0.0]),
+	Events.connect("game_over", self, "stop"),
+	Events.connect("game_restarted", self, "play", [0.0])
 	]
 
 
