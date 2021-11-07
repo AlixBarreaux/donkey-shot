2cda63e7cda346744fefd9978374c0d5f89ae775
BarreauxAlix <barreauxalix@tutanota.com>
2021-11-07 12:04:05 +0100

    Improve the OptionsMenu.

--- Scripts/GUI/Menus/options_menu.gd
+++ Scripts/GUI/Menus/options_menu.gd
@@ -5,13 +5,34 @@ extends Control
 # ----------------------------- DECLARE VARIABLES ------------------------------
 
 
+# Node Paths
+export (NodePath) var first_button_to_focus_node_path = null
+
+
+# Node References
+onready var first_button_to_focus: Button = get_node(first_button_to_focus_node_path)
+
 
 # ---------------------------------- RUN CODE ----------------------------------
 
 
+func _ready() -> void:
+	self._initialize_asserts()
+	self._initialize()
+	
+	self.hide()
+
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
 
+
+func _initialize_asserts() -> void:
+	assert(self.first_button_to_focus_node_path != null)
+	assert(self.first_button_to_focus is Button)
+
+
+func _initialize() -> void:
+	self.first_button_to_focus.grab_focus()
 
 
 func _on_BackButton_pressed() -> void:
