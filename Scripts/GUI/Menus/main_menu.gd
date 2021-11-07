9bde371d9fc39571776f29f08a01feebd1ae2a8c
BarreauxAlix <barreauxalix@tutanota.com>
2021-11-07 12:16:42 +0100

    Fix MainMenu not capturing mouse and make the OptionsMenu appear when the OptionsButton is clicked.

--- Scripts/GUI/Menus/main_menu.gd
+++ Scripts/GUI/Menus/main_menu.gd
@@ -10,7 +10,14 @@ export (NodePath) var first_button_to_focus_node_path = null
 
 
 # Node References
+onready var menus_to_display: Control = $MenusToDisplay
+
+	# Buttons
 onready var first_button_to_focus: Button = get_node(first_button_to_focus_node_path)
+onready var options_button: Button = $VBoxContainer/OptionsButton
+
+	# Menus to Display
+onready var options_menu: Control = $MenusToDisplay/OptionsMenu
 
 
 # ---------------------------------- RUN CODE ----------------------------------
@@ -31,3 +38,19 @@ func _initialize_asserts() -> void:
 	
 func _initialize() -> void:
 	self.first_button_to_focus.grab_focus()
+
+
+# Visibility Changed signals
+func _on_OptionsMenu_visibility_changed() -> void:
+	self.menus_to_display.set_mouse_filter(MOUSE_FILTER_IGNORE)
+	self.options_menu.set_mouse_filter(MOUSE_FILTER_IGNORE)
+
+	self.options_button.grab_focus()
+
+
+# Button Pressed signals
+func _on_OptionsButton_pressed() -> void:
+	menus_to_display.set_mouse_filter(MOUSE_FILTER_STOP)
+	options_menu.set_mouse_filter(MOUSE_FILTER_STOP)
+	
+	options_menu.show()
