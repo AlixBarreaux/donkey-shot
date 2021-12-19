c307a31cfbee3c70a3990b4609756ab1401af1d3
Alix <alixbarreaux@tutanota.com>
2021-12-19 04:35:28 +0100

    Rename HBoxContainer2 without the '2' int the TutorialMenu and add/fix buttons focus in the TutorialMenu and the OptionsMenu.

--- tutorial_menu.gd
+++ tutorial_menu.gd
@@ -5,18 +5,42 @@ extends Control
 # ----------------------------- DECLARE VARIABLES ------------------------------
 
 
-onready var checkbox: CheckBox = $HBoxContainer2/CheckBox
+# Node Paths
+export (NodePath) var first_button_to_focus_node_path = null
+
+
+# Node References
+	# Buttons
+onready var first_button_to_focus: Button = get_node(first_button_to_focus_node_path)
+
+onready var checkbox: CheckBox = $HBoxContainer/CheckBox
 
 
 # ---------------------------------- RUN CODE ----------------------------------
 
 
+func _ready() -> void:
+	self._initialize_asserts()
+	self._initialize()
+
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
+
+
+func _initialize_asserts() -> void:
+	assert(self.first_button_to_focus_node_path != null)
+	assert(self.first_button_to_focus is Button)
+
+
+func _initialize() -> void:
+	self.first_button_to_focus.grab_focus()
 
 
 func _on_CloseButton_pressed():
 	Settings.set_tutorial_enabled(!checkbox.pressed)
 	
+	
 	self.hide()
+	
+	
 	Events.emit_signal("game_initialized")
